b255ad9c040a3fd928446bb928b706e7164b1300
Alexey Nikolaevskiy <anikolaevski@gmail.com>
2021-03-04 22:28:43 +0300

    fixed

--- src/ItemFilter.js
+++ src/ItemFilter.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { items, filters } from './AppData';
-let CurrentSelection = 'All';
 
 export default class CBToolbar extends React.Component {
   static propTypes = {};
@@ -10,18 +9,18 @@ export default class CBToolbar extends React.Component {
   constructor(props) {
     super(props);
   }
-  callback() { 
-    this.setState( (prev) => ({prev, current: CurrentSelection}) );
+  callback(newValue) { 
+    this.setState( (prev) => ({prev, current: newValue}) );
   }
   render () {
     let k = 0;
     return (
       <div>
         <div className="es_container">
-          {filters.map( o => <Cbutton key={o} item = {o} callback = {this.callback.bind(this)} />)}
+          {filters.map( o => <Cbutton key={o} item={o} callback={this.callback.bind(this)} />)}
         </div>
         <div className="es_window">
-          {items.map( o => <CItem key={k++} item = {o}/>)}
+          {items.map( o => <CItem key={k++} item={o} state={this.state} />)}
         </div>
       </div>
     );
@@ -37,8 +36,7 @@ class Cbutton extends React.Component {
     super(props);
   }
   handleClick () {
-    CurrentSelection = this.props.item;
-    this.props.callback();
+    this.props.callback(this.props.item);
   }
   render() {
     return(
@@ -49,14 +47,15 @@ class Cbutton extends React.Component {
 
 class CItem extends React.Component {
   static propTypes = {
-    item: PropTypes.object.isRequired
+    item: PropTypes.object.isRequired,
+    state: PropTypes.object.isRequired
   }
   constructor(props) {
     super(props);
   }
   render() {
     const {img, category} = this.props.item;
-    if (CurrentSelection === 'All' || category === CurrentSelection) {
+    if (this.props.state.current === 'All' || category === this.props.state.current) {
       return( <img src={img}/> );
     } else {
       return null;
